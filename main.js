b7c96b02e10a585ae866c437ac2459506f06aeb1
ItaloNNeves <manguinhagamer7@gmail.com>
2026-04-06 19:42:21 -0300

    3-version-com-js

--- main.js
+++ main.js
@@ -10,3 +10,29 @@ let menu = document.querySelector(".lishe")
     btn.classList.toggle("active")
     menu.classList.toggle("active")
   } )
+//  Rolagem horizontal
+let cards = document.querySelector(".boxsabores")
+let esq = document.querySelector(".setleft")
+let dir = document.querySelector(".setright")
+
+dir.addEventListener( "click" , () => {
+  cards.scrollLeft += 300
+} )
+esq.addEventListener( "click" , () => {
+  cards.scrollLeft -= 300
+} )
+// Imagens banner
+let sli = document.querySelector(".imgbnn")
+let tdsli = document.querySelectorAll(".imgsbox").length
+
+let indexsli = 0
+
+setInterval(function(){  
+
+indexsli++
+
+ if ( indexsli >= tdsli){
+  indexsli = 0 
+}
+sli.style.transform = `translateX(-${indexsli * 100}%)`;
+},2000)
